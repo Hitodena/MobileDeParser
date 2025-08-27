f1b0ff093574754260fc3c362fe902fc870e94aa
Lavashina <Hitodena@ya.ru>
2025-08-27 21:18:49 +0300

    fix: script sudo docker

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,17 +2,17 @@
 
 # Остановка и удаление существующего контейнера
 
-if docker ps -a | grep -q mobilede; then
-    docker stop mobilede
-    docker rm mobilede
+if sudo docker ps -a | grep -q mobilede; then
+    sudo docker stop mobilede
+    sudo docker rm mobilede
 fi
 
 # Сборка образа
-docker build -t mobilede-parser:latest -f ../docker/Dockerfile ..
+sudo docker build -t mobilede-parser:latest -f ../docker/Dockerfile ..
 
 # Запуск контейнера с volumes
-docker run -d --name mobilede \
-  -v $(pwd)/var/www/mobile:/app/var/www/mobile \
+sudo docker run -d --name mobilede \
+  -v /var/www/mobile:/app/var/www/mobile \
   -v $(pwd)/logs:/app/logs \
   -v $(pwd)/configuration.yaml:/app/configuration.yaml \
   -v $(pwd)/proxies.txt:/app/proxies.txt \
