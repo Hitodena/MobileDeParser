2d62e87813912ade5b2b876e6b9406bde29141fa
Lavashina <Hitodena@ya.ru>
2025-08-28 11:14:36 +0300

    fix: links generation

--- shared/utils/generate_links.py
+++ shared/utils/generate_links.py
@@ -1,10 +1,16 @@
+import re
 from typing import List
 
 
 def generate_links(
     base_search_url: str, pages: int, items_per_page: int
 ) -> List[str]:
-    return [
-        f"{base_search_url},pgn:{page},pgs:{items_per_page}"
-        for page in range(1, pages + 1)
-    ]
+    url_without_pgn = re.sub(r",pgn:\d+", "", base_search_url)
+    url_without_pagination = re.sub(r",pgs:\d+", "", url_without_pgn)
+
+    links = []
+    for page in range(1, pages + 1):
+        link = f"{url_without_pagination},pgn:{page},pgs:{items_per_page}"
+        links.append(link)
+
+    return links
